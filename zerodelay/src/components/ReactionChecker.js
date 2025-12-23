e857cc4743f22a89637e7b67d974f538ad184385
TheekshR <theeksh99@gmail.com>
2025-12-23 13:00:28 +0530

    Create ReactionChecker.js

---
+++ zerodelay/src/components/ReactionChecker.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const ReactionChecker = ({ status, handleClick, trialNumber, totalTrials }) => {
+  if (status === "waiting") {
+    return <div className="reaction-box red">Wait...</div>;
+  }
+
+  if (status === "ready") {
+    return <div className="reaction-box green" onClick={handleClick}>
+      Trial {trialNumber + 1} of {totalTrials}
+    </div>;
+  }
+
+  return null;
+};
+
+export default ReactionChecker;
